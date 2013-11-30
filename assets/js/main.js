eb9e5a061389ad926ef85ef292d0135ca3152148
Yuki MIZUNO <mizuyuu0904@gmail.com>
2013-11-30 16:15:02 +0900

    first stage complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,73 +1,103 @@
-function validateNum(key, callback1, callback2, callback3) {
+$(function(){
+
+function validateNum(key, callback1, callback2, callback3){
 	var code = key.keyCode;
-	switch (code) {
-		case 8:
+	switch(code){
+		case 8: // backspace
+		case 9: // tab
 			return;
-			break;
-		case 37:
+		case 37: // left
 			callback1();
 			return;
-			break;
-		case 39:
+		case 39: // right
 			callback2();
 			return;
-			break;
-		case 9:
-			return;
-			break;
-		case 48:
-		case 49:
-		case 50:
-		case 51:
-		case 52:
-		case 53:
-		case 54:
-		case 55:
-		case 56:
-		case 57:
-			if (!key.altKey && !key.shiftKey && !key.ctrlKey) {
-				this.value = code - 48;
-				setTimeout(function(){callback2();},1);
+		case 48: // 0
+		case 49: // 1
+		case 50: // 2
+		case 51: // 3
+		case 52: // 4
+		case 53: // 5
+		case 54: // 6
+		case 55: // 7
+		case 56: // 8
+		case 57: // 9
+			if(!key.altKey && !key.shiftKey && !key.ctrlKey){
+				var val = code - 48;
+				$('input.active').addClass('hasValue');
+				setTimeout(function(){
+					callback2();
+					$('.keypad[value="' + val + '"]').addClass('active');
+				},1);
+				setTimeout(function(){
+					$('.keypad[value="' + val + '"]').removeClass('active');
+				},100);
 				return;
-				break;
 			}
 		default:
 			callback3();
 	}
 }
 
-var input = document.getElementsByTagName("input");
-var button = document.getElementsByTagName("button");
-var form = document.getElementsByTagName("form");
-var keypad = document.getElementsByClassName("keypad");
+function clearInput(){
+	$('input.active').removeClass('active');
+	$('input').addClass('invalid').addClass('hasValue');
+	setTimeout(function(){
+		$('input').val('').removeClass('hasValue');
+		$('input').first().focus();
+		$('.invalid').removeClass('invalid');		
+	},1000);
+}
 
-keypad[0].addEventListener("click", function(){
-	document.activeElement.value = this.value;
-	//document.activeElement.nextSibling().focus();
+$('.keypad').on("click", function(){
+	var currentActive = $('.active');
+	var index = parseInt(currentActive[0].id.slice(3));
+	currentActive.val($(this).val());
+	currentActive.next().focus();
+	currentActive.change();
+	if(index == 4){
+		$('form').submit();
+	}
+});
+	
+$('input').change(function(){
+	if($(this).val() != ''){
+		$(this).addClass('hasValue');
+	} else {
+		$(this).removeClass('hasValue');
+	}
 });
 
-input[0].addEventListener("keydown", function(k){
-	validateNum(k, function(){}, function(){input[1].focus();},function(){k.preventDefault()});
-});
-input[1].addEventListener("keydown", function(k){
-	validateNum(k, function(){input[0].focus()}, function(){input[2].select();},function(){k.preventDefault()});
-});
-input[2].addEventListener("keydown", function(k){
-	validateNum(k, function(){input[1].focus()}, function(){input[3].focus();},function(){k.preventDefault()});
-});
-input[3].addEventListener("keydown", function(k){
-	validateNum(k, function(){input[2].focus()}, function(){button[0].focus();},function(){k.preventDefault()});
+$('input').on("keydown", function(k){
+	var index = parseInt(this.id.slice(3));
+	var c1 = index > 1 ? function(){$('input')[index-2].focus()} : null;
+	var c2 = index < 4 ? function(){$('input')[index].focus()} : function(){setTimeout(function(){$('form').submit();},1)};
+	validateNum(k, c1, c2,function(){k.preventDefault();});
 });
 
-form[0].addEventListener("submit", function(){
-	var value = input[0].value * 1000 + 
-				input[1].value * 100 + 
-				input[2].value * 10 + 
-				input[3].value * 1; 
+$('input').focus(function(){
+	$('.active').removeClass('active');
+	$(this).addClass('active');
+});
 
-	if(parseInt(value) == 6553) {
-		form.action = "128374/complete.html";
+$('form').submit(function(){
+	var value = '';
+	$('input[type="tel"]').each(function(item){
+		value += $(this).val();
+	});
+
+	if(parseInt(value) == 1235){
+		$('output').text(''); // comming soon.
+		console.log('complete');
+		$('input').addClass('active');
+		alert('Tweet: @mzyy94 I want a cookie ;( ' + (1 - new Date()));
+		$('input').val('').removeClass('active').removeClass('hasValue');
+		$('input').first().focus();
+		return false;
+	} else {
+		clearInput();
 		return false;
 	}
 });
 
+});
