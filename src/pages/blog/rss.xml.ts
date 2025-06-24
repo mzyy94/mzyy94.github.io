183a9b287aad70f8c2ad74ff6e7f0acd777c0465
Yuki MIZUNO <mizuyuu0904@gmail.com>
2025-06-24 09:23:27 +0900

    Update src/pages

--- src/pages/blog/rss.xml.ts
+++ src/pages/blog/rss.xml.ts
@@ -1,26 +1,25 @@
+import type { APIRoute } from 'astro';
 import rss from "@astrojs/rss";
 import { getSinglePage } from "@/lib/contentParser.astro";
-import { getDescriptions } from "@/lib/utils/contentDescription";
 import config from "@/config/config.json";
 
-const moreText = "<br/><br/>続きはWebで";
-export async function get(context) {
-  const posts = await getSinglePage("posts");
-  const contents = await getDescriptions(posts);
+const moreText = "…<br/><br/>続きはWebで";
+export const GET: APIRoute = async (context) => {
+  const posts = await getSinglePage("blog");
   return rss({
     title: config.site.title,
     description: config.site.description,
     // Pull in your project "site" from the endpoint context
     // https://docs.astro.build/en/reference/api-reference/#contextsite
     site: context.site,
-    items: posts.map((post, i) => ({
-      content: contents[i] ? contents[i] + moreText : post.body,
+    items: posts.map((post) => ({
+      content: post.body?.slice(0, 512) + moreText || "",
       title: post.data.title,
       description: post.data.description,
       pubDate: post.data.date,
       categories: post.data.categories,
       author: post.data.author,
-      link: `/blog/${post.slug}/`,
+      link: `/blog/${post.id}/`,
     })),
   });
 }
